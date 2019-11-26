558917c333185261b50a6eb52361381054739d65
Pranav <pranav.ji@live.in>
2019-11-26 22:50:04 +0530

    added validatioins

--- src/test/java/com/igrecsys/proputil/TestPropertyUtil.java
+++ src/test/java/com/igrecsys/proputil/TestPropertyUtil.java
@@ -14,15 +14,15 @@ public class TestPropertyUtil {
     @Test
     public void testCompare() {
         String prop1 =
-                "title\t = \tDigital Document and Filing System (DDFS)\tNumérique de documents et système de classement (DDFS)\n" +
-                        "greeting\t = \tGreetings, it is now\tBonjour, il est maintenant\n" +
-                        "common.ddfsTitle\t = \tDigital Document Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\n" +
+                "title\t = \tDigital Document and Filing System (DDFS)\tNumérique de documents et système de classement (DDFS)\r\n" +
+                        "greeting\t = \tGreetings, it is now\tBonjour, il est maintenant\r\n" +
+                        "common.ddfsTitle\t = \tDigital Document Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\r\n" +
                         "common.status.active\t = \tActive\tactif";
         String prop2 =
-                "common.ddfsTitle\t = \tDigital govind Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\n" +
-                        "common.status.active\t = \tActive\tactif\n" +
-                        "common.designation\t = \tDesignation\tLa désignation\n" +
-                        "common.mail\t = \tMail\tCourrier\n" +
+                "common.ddfsTitle\t = \tDigital govind Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\r\n" +
+                        "common.status.active\t = \tActive\tactif\r\n" +
+                        "common.designation\t = \tDesignation\tLa désignation\r\n" +
+                        "common.mail\t = \tMail\tCourrier\r\n" +
                         "common.sex.female\t = \tFemale\tFemme";
         DtoCompareResult result = null;
         try {
@@ -36,13 +36,13 @@ public class TestPropertyUtil {
         assertEquals(result.getResultMessage1(), "2 properties found in Property File 1 that is not in Property File 2 ");
         assertEquals(result.getResultMessage2(), "3 properties found in Property File 2 that is not in Property File 1 ");
         assertEquals(result.getResultMessage3(), "1 properties have difference in content ");
-        assertEquals(result.getDifferenceF1F2(), "title  =  Digital Document and Filing System (DDFS)\tNumérique de documents et système de classement (DDFS)\n" +
-                "greeting  =  Greetings, it is now\tBonjour, il est maintenant\n");
-        assertEquals(result.getDifferenceF2F1(), "common.mail  =  Mail\tCourrier\n" +
-                "common.sex.female  =  Female\tFemme\n" +
-                "common.designation  =  Designation\tLa désignation\n");
-        assertEquals(result.getDifferenceContentF1(), "common.ddfsTitle  =  Digital Document Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\n");
-        assertEquals(result.getDifferenceContentF2(), "common.ddfsTitle  =  Digital govind Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\n");
+        assertEquals(result.getDifferenceF1F2(), "title  =  Digital Document and Filing System (DDFS)\tNumérique de documents et système de classement (DDFS)\r\n" +
+                "greeting  =  Greetings, it is now\tBonjour, il est maintenant\r\n");
+        assertEquals(result.getDifferenceF2F1(), "common.mail  =  Mail\tCourrier\r\n" +
+                "common.sex.female  =  Female\tFemme\r\n" +
+                "common.designation  =  Designation\tLa désignation\r\n");
+        assertEquals(result.getDifferenceContentF1(), "common.ddfsTitle  =  Digital Document Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\r\n");
+        assertEquals(result.getDifferenceContentF2(), "common.ddfsTitle  =  Digital govind Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\r\n");
         assertEquals(prop1, result.getPropertyFile1());
         assertEquals(prop2, result.getPropertyFile2());
     }
@@ -51,15 +51,15 @@ public class TestPropertyUtil {
     public void testMerge() {
 
         String prop1 =
-                "title\t = \tDigital Document and Filing System (DDFS)\tNumérique de documents et système de classement (DDFS)\n" +
-                        "greeting\t = \tGreetings, it is now\tBonjour, il est maintenant\n" +
-                        "common.ddfsTitle\t = \tDigital Document Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\n" +
+                "title\t = \tDigital Document and Filing System (DDFS)\tNumérique de documents et système de classement (DDFS)\r\n" +
+                        "greeting\t = \tGreetings, it is now\tBonjour, il est maintenant\r\n" +
+                        "common.ddfsTitle\t = \tDigital Document Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\r\n" +
                         "common.status.active\t = \tActive\tactif";
         String prop2 =
-                "common.ddfsTitle\t = \tDigital govind Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\n" +
-                        "common.status.active\t = \tActive\tactif\n" +
-                        "common.designation\t = \tDesignation\tLa désignation\n" +
-                        "common.mail\t = \tMail\tCourrier\n" +
+                "common.ddfsTitle\t = \tDigital govind Filing System (DDFS)\tDépôt du document numérique Système (DDFS)\r\n" +
+                        "common.status.active\t = \tActive\tactif\r\n" +
+                        "common.designation\t = \tDesignation\tLa désignation\r\n" +
+                        "common.mail\t = \tMail\tCourrier\r\n" +
                         "common.sex.female\t = \tFemale\tFemme";
 
         DtoMergeResult result = null;
