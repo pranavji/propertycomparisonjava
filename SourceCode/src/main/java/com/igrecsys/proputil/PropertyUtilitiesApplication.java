5ba3d0859c1ccddc47c0044f22e94843630df43a
Pranav <pranav.ji@live.in>
2019-12-01 17:20:45 +0530

    Added automatic folder creation.

--- SourceCode/src/main/java/com/igrecsys/proputil/PropertyUtilitiesApplication.java
+++ SourceCode/src/main/java/com/igrecsys/proputil/PropertyUtilitiesApplication.java
@@ -3,12 +3,16 @@ package com.igrecsys.proputil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class PropertyUtilitiesApplication {
 
     private int maxUploadSizeInMb = 200 * 1024 * 1024; // 10 MB
 
 	public static void main(String[] args) {
+
+		new File("files").mkdir();
 		SpringApplication.run(PropertyUtilitiesApplication.class, args);
 
     }
