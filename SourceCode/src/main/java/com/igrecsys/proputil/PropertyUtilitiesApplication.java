d5b499ed45d907b5d6d7d1b1e47d0541db012cda
Pranav <pranav.ji@live.in>
2019-12-01 16:32:46 +0530

    Added upload functionality

--- SourceCode/src/main/java/com/igrecsys/proputil/PropertyUtilitiesApplication.java
+++ SourceCode/src/main/java/com/igrecsys/proputil/PropertyUtilitiesApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PropertyUtilitiesApplication {
 
+    private int maxUploadSizeInMb = 200 * 1024 * 1024; // 10 MB
+
 	public static void main(String[] args) {
 		SpringApplication.run(PropertyUtilitiesApplication.class, args);
-	}
+
+    }
+
+
 
 }
--- SourceCode/src/main/java/com/igrecsys/proputil/Utils/PropertyUtil.java
+++ SourceCode/src/main/java/com/igrecsys/proputil/Utils/PropertyUtil.java
@@ -2,9 +2,10 @@ package com.igrecsys.proputil.Utils;
 
 import com.igrecsys.proputil.dto.DtoCompareResult;
 import com.igrecsys.proputil.dto.DtoMergeResult;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
+import java.io.*;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Properties;
@@ -37,8 +38,8 @@ public class PropertyUtil {
         f1f2keys.removeAll(file2Keys);
         f2f1keys.removeAll(file1Keys);
 
-        f1f2keys.forEach(k -> outputf1f2.setProperty(k.toString(), file1.getProperty(k.toString())));
-        f2f1keys.forEach(k -> outputf2f1.setProperty(k.toString(), file2.getProperty(k.toString())));
+        f1f2keys.forEach(k -> setToProperty(k, file1.getProperty(k.toString()), outputf1f2));
+        f2f1keys.forEach(k -> setToProperty(k, file2.getProperty(k.toString()), outputf2f1));
 
 
         Set<Object> interseciont = file1Keys
@@ -56,15 +57,76 @@ public class PropertyUtil {
         String message3 = interseciont.size() + " properties have difference in content ";
 
 
-        interseciont.forEach(q -> compareResultF1.setProperty(q.toString(), file1.getProperty(q.toString())));
-        interseciont.forEach(q -> compareResultF2.setProperty(q.toString(), file2.getProperty(q.toString())));
+        // interseciont.forEach(q -> compareResultF1.setProperty(q.toString(), file1.getProperty(q.toString())));
+        interseciont.forEach(q -> setToProperty(q, file1.getProperty(q.toString()), compareResultF1));
+        interseciont.forEach(q -> setToProperty(q, file2.getProperty(q.toString()), compareResultF2));
+        //  interseciont.forEach(q -> compareResultF2.setProperty(q.toString(), file2.getProperty(q.toString())));
 
 
-        return new DtoCompareResult(getPropertyAsString(outputf1f2), getPropertyAsString(outputf2f1), getPropertyAsString(compareResultF1), getPropertyAsString(compareResultF2), src1, src2, message1, message2, message3);
+        String fName = "files/t";
+        String fNameF1F2 = "files/t1";
+        String fNameF2F1 = "files/t2";
+        String fNameCompareFile = "files/t3";
+        String fNameF2 = "files/t4";
+
+        Boolean exist = true;
+        File outputFile;
+
+        while (exist) {
+
+            fName = "files/Output" + System.currentTimeMillis();
+            fNameF1F2 = fName + "F1F2.properties";
+            fNameF2F1 = fName + "F2F1.properties";
+            fNameCompareFile = fName + "F1.properties";
+            fNameF2 = fName + "F2.properties";
+
+            outputFile = new File(fNameF1F2);
+            if (!outputFile.createNewFile())
+                continue;
+
+            outputFile = new File(fNameF2F1);
+            if (!outputFile.createNewFile())
+                continue;
+            outputFile = new File(fNameCompareFile);
+            if (!outputFile.createNewFile())
+                continue;
+
+            exist = false;
+        }
+
+        OutputStreamWriter outputStream;
+        outputStream = new OutputStreamWriter(new FileOutputStream(fNameF1F2), "UTF-8");
+        outputf1f2.store(outputStream, message1);
+        outputStream.close();
+        outputStream = new OutputStreamWriter(new FileOutputStream(fNameF2F1), "UTF-8");
+        outputf2f1.store(outputStream, message2);
+        outputStream.close();
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fNameCompareFile), "UTF-8"));
+
+        bufferedWriter.write("#" + message3);
+        compareResultF1.forEach((k, p) -> {
+            try {
+                bufferedWriter.write("\r\n" + k + " = " + p + "     |  " + compareResultF2.getProperty(k.toString()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        bufferedWriter.flush();
+        bufferedWriter.close();
+
+
+        return new DtoCompareResult(getPropertyAsString(outputf1f2), getPropertyAsString(outputf2f1), getPropertyAsString(compareResultF1), getPropertyAsString(compareResultF2), src1, src2, message1, message2, message3, fNameF1F2, fNameF2F1, fNameCompareFile);
 
 
     }
 
+    private static void setToProperty(Object k, String property, Properties outputf1f2) {
+        if (null == property)
+            property = "";
+        if (k.toString().trim().length() != 0)
+            outputf1f2.setProperty(k.toString(), property);
+    }
 
     public static DtoMergeResult merge(String src1, String src2) throws Exception {
         LinkedHashMap<String, String> file1 = new LinkedHashMap<String, String>();
@@ -117,7 +179,7 @@ public class PropertyUtil {
                 } else {
                     file2.put("comment" + i, line);
                     i++;
-                    System.out.println("comment " + line);
+                    // System.out.println("comment " + line);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -141,11 +203,41 @@ public class PropertyUtil {
                 mergeResult.append(key + "  =  " + output.get(key) + "\n");
         }
 
+        Boolean exist = true;
+        File outputFile;
+        String fName = "files/t";
 
-        return new DtoMergeResult(mergeResult.toString(), src1, src2, message);
+        while (exist) {
+
+            fName = "files/Output" + System.currentTimeMillis() + ".properties";
+
+            outputFile = new File(fName);
+            if (!outputFile.createNewFile())
+                continue;
+
+
+            exist = false;
+        }
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fName), "UTF-8"));
+
+
+        for (String key : output.keySet()) {
+
+            if (key.startsWith("comment"))
+                bufferedWriter.write(output.get(key) + "\r\n");
+            else
+                bufferedWriter.write(key + "  =  " + output.get(key) + "\r\n");
+        }
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+
+        return new DtoMergeResult(mergeResult.toString(), src1, src2, message, fName);
 
 
     }
+
 
     private static String getPropertyAsString(Properties prop) {
 
@@ -156,8 +248,50 @@ public class PropertyUtil {
 
     private static String getFormattedProperty(String p, String k) {
         return p + "  =  " + k
-              .replaceAll("\"", "\\\\u201C")
+                .replaceAll("\"", "\\\\u201C")
                 .replaceAll("'", "\\\\u2019")
                 .trim() + "\r\n";
+    }
+
+    public static String multipartToString(MultipartFile file) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line = reader.readLine();
+
+        while (null != line) {
+            sb.append(line + "\r\n");
+            line = reader.readLine();
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String processRequest(String src1, String src2, Model model, String mode) {
+        if (mode.equals("compare")) {
+
+            DtoCompareResult dtoCompareResult = null;
+            try {
+                dtoCompareResult = PropertyUtil.compare(src1, src2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            model.addAttribute("dataresult", dtoCompareResult);
+
+            return "compareUpload";
+
+        } else {
+
+            DtoMergeResult dtoMergeResult;
+            dtoMergeResult = null;
+            try {
+                dtoMergeResult = PropertyUtil.merge(src1, src2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            model.addAttribute("dataresult", dtoMergeResult);
+
+
+            return "mergeUpload";
+        }
     }
 }
